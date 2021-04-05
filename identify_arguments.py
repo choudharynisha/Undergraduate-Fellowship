024e529dbd6d6761dcc7837c10dde7eb669166d2
Nisha Choudhary <choudharynisha024@gmail.com>
2021-04-05 16:26:01 -0400

    Updated the Help Message for Each Argument

--- identify_arguments.py
+++ identify_arguments.py
@@ -10,19 +10,19 @@ import sys
 
 def parse_args():
     """Parses the command line arguments provided by the user in order to only look at the trigrams
-       without stopwords
+       without stop words
     
     Returns:
         str: The name of the input file with all of the trigrams, including ones with stop words
         str: The name of the output file to store all of the trigrams that don't have stop words
     """
     parser = argparse.ArgumentParser(description = "Takes in the input and the output file names")
-    parser.add_argument("-i", "--input", type = str, help = "the name of the input file",\
-        default = None, metavar = "")
-    parser.add_argument("-o", "--output", type = str, help = "the name of the output file",\
-        default = None, metavar = "")
-    parser.add_argument("-v", "--verb", type = str, help = "the main verb being looked at",\
-        default = None, metavar = "")
+    parser.add_argument("-i", "--input", type = str, default = None, metavar = "",\
+        help = "the name of the input file preceded by its relative path (if needed)")
+    parser.add_argument("-o", "--output", type = str, default = None, metavar = "",\
+        help = "the name of the output file preceded by its relative path (if needed)")
+    parser.add_argument("-v", "--verb", type = str, default = None, metavar = "",\
+        help = "the main verb being looked at")
     arguments = parser.parse_args()
     
     if(arguments.input == None):
